6433c3907e330831268d06b1a0aea98dbb698830
eyesonlyhack <eyesonlyhack@gmail.com>
2013-02-23 20:11:54 +0200

    initial

---
+++ src/alienprodigysoftware/examples/Main.java
@@ -0,0 +1,15 @@
+package alienprodigysoftware.examples;
+
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
