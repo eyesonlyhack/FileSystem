8142505dc7e3bfddd47103d5afdc1c5e7615007b
eyesonlyhack <eyesonlyhack@gmail.com>
2013-02-23 20:20:25 +0200

    second commit

---
+++ src/alienprodigysoftware/Interfaces/IFileStore.java
@@ -0,0 +1,6 @@
+package alienprodigysoftware.Interfaces;
+
+public interface IFileStore 
+{
+	boolean BackupTo(IFileStore fileStore);
+}
---
+++ src/alienprodigysoftware/filemanagement/LocalFileStore.java
@@ -0,0 +1,15 @@
+package alienprodigysoftware.filemanagement;
+
+import alienprodigysoftware.Interfaces.IFileStore;
+
+public class LocalFileStore implements IFileStore
+{
+
+	@Override
+	public boolean BackupTo(IFileStore fileStore)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
